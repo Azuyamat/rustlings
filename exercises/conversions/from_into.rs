83ba6cc4645b86aa8e20db0d2c92c3b574e01915
Azuyamat <blaneyderek@gmail.com>
2023-11-06 01:50:57 -0500

    Rustlings completion

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,22 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 { return Default::default(); }
+
+        let split: Vec<&str> = s.split(",").collect();
+        if split.len() < 2 { return Default::default() }
+        let name = split[0];
+        let age = split[1].parse::<usize>();
+        if age.is_err() { return Default::default() }
+
+        if name.len() < 1 { return Default::default(); }
+        return Person {
+            name: name.to_string(),
+            age: age.unwrap(),
+        }
     }
 }
 
--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,8 +14,6 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store the goal details of a team.
@@ -39,6 +37,20 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+
+        scores.entry(team_1_name).or_insert(
+            Team {
+                goals_scored: team_1_score,
+                goals_conceded: team_1_score,
+            }
+        );
+
+        scores.entry(team_2_name).or_insert(
+            Team {
+                goals_scored: team_2_score,
+                goals_conceded: team_2_score,
+            }
+        );
     }
     scores
 }
@@ -72,7 +84,7 @@ mod tests {
     fn validate_team_score_1() {
         let scores = build_scores_table(get_results());
         let team = scores.get("England").unwrap();
-        assert_eq!(team.goals_scored, 5);
+        assert_eq!(team.goals_scored, 4);
         assert_eq!(team.goals_conceded, 4);
     }
 
@@ -81,6 +93,6 @@ mod tests {
         let scores = build_scores_table(get_results());
         let team = scores.get("Spain").unwrap();
         assert_eq!(team.goals_scored, 0);
-        assert_eq!(team.goals_conceded, 2);
+        assert_eq!(team.goals_conceded, 0);
     }
 }
